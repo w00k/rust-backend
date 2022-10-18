592e9bcc5c14f948f248861219e0cdc942e07561
Francisco Arenas <franciscoarenas.globant@latam.com>
2022-10-17 23:10:40 -0300

    index

--- blog-platzi/src/main.rs
+++ blog-platzi/src/main.rs
@@ -14,13 +14,15 @@ use std::env;
 use crate::models::PostSimple;
 
 // pool de conexiones a la base de datos
-use diesel::r2d2::{self, ConnectionManager};
+use diesel::r2d2::{self, ConnectionManager, PooledConnection};
 use diesel::r2d2::Pool;
 
 // actix framework para lo que es servicios web
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-// Importamos esquemas de BBDD y modelos
+pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
+// Importamos esquemas de BBDD y modelo 
 pub mod models;
 pub mod schema;
 
@@ -28,14 +30,13 @@ pub mod schema;
 
 // establish_connection: lee la variable en .env file y crea la conexión a la base de datos
 // @param: none
-// return PgConnection
-pub fn establish_connection() -> PgConnection {
+// return Pool<ConnectionManager<PgConnection>>
+pub fn establish_connection() -> Pool<ConnectionManager<PgConnection>>{
     dotenv().ok();
-
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
-    PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+    let connection = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = Pool::builder().build(connection).expect("No se puedo construir el pool de conexiones");
+    return pool;
 }
 
 // hello_world: return a "Hello world" message
@@ -43,20 +44,32 @@ pub fn establish_connection() -> PgConnection {
 // return HttpResponse::Ok().body("Hello world");
 // ó
 // HttpResponse::Ok().body("Hello world")
-#[get("/hello")]
-async fn hello_world() -> impl Responder {
-    return HttpResponse::Ok().body("Hello world");
+#[get("/")]
+async fn index(pool: web::Data<DbPool>) -> impl Responder {
+    use self::models::Post;
+    use self::schema::posts::dsl::*;
+
+    let mut conn = pool.get().expect("Problemas al traer la base de datos");
+
+    // en el treads qn que estamos lo bloquea para no generar condición de carrera
+    return match web::block(move || {posts.load::<Post>(&mut conn)}).await {
+        Ok(app_data) => HttpResponse::Ok().body("Hemos encontrado algo"),
+        Err(err) => HttpResponse::Ok().body("Error al recibir la data")
+    };
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
-    dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let connection = ConnectionManager::<PgConnection>::new(database_url);
-    let pool = Pool::builder().build(connection).expect("No se puedo construir el pool de conexiones");
+    // dotenv().ok();
+    // let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    // let connection = ConnectionManager::<PgConnection>::new(database_url);
+    // let pool = Pool::builder().build(connection).expect("No se puedo construir el pool de conexiones");
 
-    HttpServer::new(move || App::new().service(hello_world).app_data(pool.clone()))
+    let pool = establish_connection();
+
+    // move indicamos que el ownership a donde lo vamos a necesitar, en este caso es el pool de conexiones
+    HttpServer::new(move || App::new().service(index).app_data(web::Data::new(pool.clone())))
         .bind(("localhost", 9900))?
         .run()
         .await
